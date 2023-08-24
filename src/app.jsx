b4001857c96c87df3cf272b52ea3a4a9af2f6bef
subinqkim <subinlk@uci.edu>
2023-08-23 19:51:28 -0700

    React component that displays pic/user info. The 'useState' hook manages state of fetched user info.

---
+++ src/app.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+import Picture from './Picture';
+import UserInfo from './UserInfo';
+
+const App = () => {
+  const [userInfo, setUserInfo] = useState(null);
+
+  const handleImageClick = async () => {
+    try {
+      const response = await fetch('your-api-endpoint-url');
+      const data = await response.json();
+      setUserInfo(data); // Update state with fetched data
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
+
+  return (
+    <div>
+      <Picture onClick={handleImageClick} />
+      {userInfo && <UserInfo data={userInfo} />}
+    </div>
+  );
+};
+
+export default App;
