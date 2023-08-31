46f96ca0ee564a09037909b111c0b6da9b6dbc37
subinqkim <subinlk@uci.edu>
2023-08-31 14:57:56 -0700

    Updating API fetch in my component, now using Redux Toolkit for fetch/store of API data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setApiData } from './apiDataSlice';
 import Picture from './picture';
 import UserInfo from './UserInfo';
 
 const App = () => {
-  const [userInfo, setUserInfo] = useState(null);
+  const dispatch = useDispatch();
+  const apiData = useSelector((state) => state.apiData);
+
+  //const [userInfo, setUserInfo] = useState(null);
 
   const handleImageClick = async () => {
     console.log('Image clicked');
     try {
       const response = await fetch('http://localhost:7001/awesome/applicant');
       const data = await response.json();
-      setUserInfo(data); // Update state with fetched data
+      dispatch(setApiData(data));
+      //setUserInfo(data); // Update state with fetched data *update now replaced by redux
     } catch (error) {
       console.error('Error fetching data:', error);
     }
@@ -19,7 +25,7 @@ const App = () => {
   return (
     <div>
       <Picture onClick={handleImageClick} />
-      {userInfo && <UserInfo data={userInfo} />}
+      {apiData && <UserInfo data={apiData} />}
     </div>
   );
 };
