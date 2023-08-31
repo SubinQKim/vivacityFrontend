c0e159f62f9bea60372fc152eaed82c1c70f570b
subinqkim <subinlk@uci.edu>
2023-08-30 18:04:36 -0700

    Redux dependencies

--- src/UserInfo.jsx
+++ src/UserInfo.jsx
@@ -4,6 +4,7 @@ const UserInfo = ({ data }) => {
   return (
     <div>
       <p>Name: {data.name}</p>
+      <p>Hobbies: {data.hobbies[0]}</p>
       <p>Favorite: {data.favoriteCoffee}</p>
       {/* Display more information as needed */}
     </div>
