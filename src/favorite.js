934ba2aae3e65c779b78920869dcb43f17171f7a
subinqkim <subinlk@uci.edu>
2023-09-03 22:36:29 -0700

    Connecting favorite component to Redux store using useSelector and useDDispatch Hooks

---
+++ src/favorite.js
@@ -0,0 +1,26 @@
+// Favorites.js
+
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { addFavorite, removeFavorite } from './favoritesSlice';
+
+const Favorites = () => {
+  // Implement the UI for managing and displaying favorite hobbies here
+  const favorites = useSelector((state) => state.favorites);
+  const dispatch = useDispatch();
+
+  const handleAddFavorite = (hobby) => {
+    dispatch(addFavorite(hobby));
+  };
+
+  const handleRemoveFavorite = (hobby) => {
+    dispatch(removeFavorite(hobby));
+  };
+  return (
+    <div>
+      {/* UI for favorite hobbies */}
+    </div>
+  );
+};
+
+export default Favorites;
