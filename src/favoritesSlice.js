02346897f664f7c90155607bec8273a814dda10c
subinqkim <subinlk@uci.edu>
2023-09-10 18:44:18 -0700

    Console debug statements for favorite slice

--- src/favoritesSlice.js
+++ src/favoritesSlice.js
@@ -9,6 +9,7 @@ const favoritesSlice = createSlice({
     addHobby: (state, action) => {
       // Implement adding a favorite hobby to the state
       const hobbyToAdd = action.payload;
+      console.log('Adding hobby:', hobbyToAdd);
       if (!state.includes(hobbyToAdd)) {
         state.push(hobbyToAdd);
       }
@@ -16,6 +17,7 @@ const favoritesSlice = createSlice({
     removeHobby: (state, action) => {
       // Implement removing a favorite hobby from the state
       const hobbyToRemove = action.payload;
+      console.log('Removing hobby:', hobbyToRemove);
       const index = state.indexOf(hobbyToRemove);
       if (index !== -1) {
         state.splice(index, 1);
